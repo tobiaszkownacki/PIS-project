3b08bffde37e1fc0d7f2d3131a951f5432ab9d86
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2025-11-08 19:10:20 +0100

    created backend skeleton. Configured connection between backend and database.

---
+++ HireMeBackend/src/test/java/com/hiremeapp/backend/HireMeBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hiremeapp.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+class HireMeBackendApplicationTests {
+
+    @Test
+    void TestExample() {
+        assertEquals(2 + 2, 4);
+    }
+
+}
